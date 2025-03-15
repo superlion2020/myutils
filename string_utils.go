7e28fa5825606e9021afc6c27b94af5227025af0
laoma <laomaaabbccd@gmail.com>
2025-03-15 22:30:49 +0800

    string_utils.go

---
+++ string_utils.go
@@ -0,0 +1,19 @@
+package myutils
+
+import (
+	"strings"
+)
+
+// ReverseString 反转字符串
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+// ContainsIgnoreCase 忽略大小写检查字符串是否包含子串
+func ContainsIgnoreCase(s, substr string) bool {
+	return strings.Contains(strings.ToLower(s), strings.ToLower(substr))
+}
